007f70f659043b74c61abdd638ec413ccd482f01
Spyion <m4tm4t380@gmail.com>
2019-03-11 11:39:34 +0100

    Ragdoll physics and bone animation

---
+++ Assets/Scripts/ArmCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmCollision : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] objects;
+    [SerializeField]
+    private GameObject[] objectsToIgnore;
+
+    void Awake()
+    {
+        for (int i = 0; i < objects.Length; i++)
+        {
+            for (int j = 0; j < objectsToIgnore.Length; j++)
+            {
+                Physics2D.IgnoreCollision(objects[i].GetComponent<Collider2D>(), objectsToIgnore[j].GetComponent<Collider2D>());
+            }
+        }
+    }
+}
